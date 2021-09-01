5557fa5ad2a6c962ca2e3aeb742ce1dced39e10b
yaroamaro219 <yaroamari@gmail.com>
2021-09-01 10:44:30 -0400

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,6 @@ export default class Home1 extends Component {
     return (
       <>
       <div class='home-container'>
-         {/* <div class="vimeo-wrapper">
-          <iframe src="https://player.vimeo.com/video/491371357?background=1&autoplay=1&loop=1&byline=0&title=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-        </div> */}
           <div class='home-background'>
             
           </div>
